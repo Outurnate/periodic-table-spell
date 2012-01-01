949e9535c5690f131b2ca140ea139065e0f48180
Joseph Dillon <joe@w00t42.net>
2011-12-31 23:33:57 -0500

    adding logic

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
 using System;
+using System.Xml;
 
 namespace Spell
 {
-
   static class Program
   {
     public static void Main()
     {
       PeriodicTable table = new PeriodicTable();
-      Console.WriteLine(table.GetAtoms());
+      table.Init();
+    }
+  }
+
+  class PeriodicTable
+  {
+    struct Element
+    {
+      string Name;
+      string Symbol;
+      int Atomic;
+    }
+
+    periodictable table;
+    Element[] elements;
+
+    public PeriodicTable()
+    {
+      table = new periodictable();
+    }
+
+    public void Init()
+    {
+      XmlDocument resolver = new XmlDocument();
+      resolver.LoadXml(table.GetAtoms());
     }
   }
 }
