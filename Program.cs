ab14baa52a272e28b420a5b8d7300f7c11a20a7d
Joseph Dillon <joe@w00t42.net>
2011-12-31 23:22:05 -0500

    generating wsdl

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace Spell
 {
+
   static class Program
   {
     public static void Main()
     {
-      
+      PeriodicTable table = new PeriodicTable();
+      Console.WriteLine(table.GetAtoms());
     }
   }
 }
