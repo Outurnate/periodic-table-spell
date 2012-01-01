14c48fe38054ff508d073e017fa71ab55b41d5dc
Joseph Dillon <joe@w00t42.net>
2012-01-01 00:00:09 -0500

    starting spell logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
+/*
+    periodic-table-spell, useless program
+    Copyright (C) 2011 Joseph Dillon
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 
 namespace Spell
@@ -10,6 +28,7 @@ namespace Spell
     {
       PeriodicTable table = new PeriodicTable();
       table.Init();
+      table.Spell("bacon");
     }
   }
 
@@ -43,6 +62,23 @@ namespace Spell
 	element.Atomic = int.Parse(resolver.GetElementsByTagName("AtomicNumber")[0].InnerXml);
       }
       this.elements = elements.ToArray();
+    }
+
+    public void Spell(string word)
+    {
+      List<int> indexes = new List<int>();
+      for (int i = 0; i < word.Length; i++)
+        indexes.Add(i);
+      foreach (Element element in elements)
+	if (word.Contains(element.Symbol))
+	  for (int i = 0; i < element.Symbol.Length; i++)
+	  {
+	    try
+	    {
+	      indexes.Remove(i);
+	    }
+            catch (Exception) { }
+	  }
     }
   }
 }
